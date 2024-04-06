1678059a4248902d7031ce10a292b67ca44f72c9
Ayush Katre <151609866+AyushKatre05@users.noreply.github.com>
2024-04-06 17:30:51 +0530

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -8,8 +8,8 @@ import numpy as np
 import random
 import string # to process standard python strings
 
-f=open('C:/Users/ayush/OneDrive/Desktop/AI ChatBot/symptom.txt','r',errors = 'ignore')
-m=open('C:/Users/ayush/OneDrive/Desktop/AI ChatBot/pincodes.txt','r',errors = 'ignore')
+f=open('symptom.txt','r',errors = 'ignore')
+m=open('pincodes.txt','r',errors = 'ignore')
 checkpoint = "./chatbot_weights.ckpt"
 #session = tf.InteractiveSession()
 #session.run(tf.global_variables_initializer())
